e3bd978c32e68e731996cecd158ac508cf8b750c
Rohan-Gunaga <rohangunaga953@gmail.com>
2026-09-24 20:58:20 +0530

    solve remove element problem

---
+++ 01_Arrays/remove_element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+
+        k = 0
+
+        for num in nums:
+            if num != val:
+                nums[k] = num
+                k += 1
+
+        return k
+
+
+        
+    
+        
